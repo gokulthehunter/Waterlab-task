8bb14f0f00f213cd21ea0ecd7ca50a18e292847c
admin <admin@admins-MacBook-Pro.local>
2020-07-21 20:01:01 +0530

    disabled redux dev tools and thunk

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import {createStore,applyMiddleware,compose} from 'redux';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 import rootReducer from './reducers'
-const middleware = [thunk]
+// const middleware = [thunk]
 
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // applyMiddleware(...middleware),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
 
